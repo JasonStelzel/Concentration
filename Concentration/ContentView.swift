df1a4f8c7f09cbfb7b2ad2886ab87258bdea0329
Jason Stelzel <laptop@epigraf.com>
2022-07-27 13:34:43 -0700

    Build 2-sided flippable CardView array using SwiftUI with filled stroked Rectangle and Text alternative sf-symbols with comments

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -8,68 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚁", "🚀", "✈️", "⛴"]
+    @State var emojiCount = 4 // inference makes this an Int
+    
     var body: some View {
-// closing braces are down below so you can try each Zstack
-// 5th phase (most reduced by removing return and parens)
-        ZStack {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 3)
-
-            Text("Hello, Jason!")
-
+        VStack {
+            HStack {
+    //            CardView(isFaceUp: false) // Must provide default if value not provided in declaration of isFaceUp in CardView below
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+                
+            }
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
-
-// 4th phase (reduce more by using default parameters)
-//        return ZStack() {
-//            RoundedRectangle(cornerRadius: 25)
-//                .stroke(lineWidth: 3)
-//
-//            Text("Hello, Jason!")
-//
-//        }
-//        .padding(.horizontal)
-//        .foregroundColor(.red)
-
-       
-// 3rd phase (reduce with hanging closure)
-//        return ZStack(content: {
-//            RoundedRectangle(cornerRadius: 25)
-//                .stroke(lineWidth: 3)
-//
-//            Text("Hello, Jason!")
-//
-//        })
-//        .padding(.horizontal)
-//        .foregroundColor(.red)
-
-  
-// 2nd phase (add Zstack)
-//        return ZStack(alignment: .center, content: {
-//            RoundedRectangle(cornerRadius: 25)
-//                .stroke(lineWidth: 3)
-//
-//            Text("Hello, Jason!")
-//
-//        })
-//        .padding(.horizontal)
-//        .foregroundColor(.red)
-
-        
-// 1st phase of development
-//        return RoundedRectangle(cornerRadius: 25)
-//            .stroke(lineWidth: 10)
-//            .padding(.horizontal)
-//            .foregroundColor(.orange)
-//
-//        Text("Hello, Jason!").foregroundColor(.orange).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+    
+    
+//    Button(action: <#T##() -> Void#>, label: <#T##() -> Label#>)
+    var add: some View {
+            Button(action: {
+            emojiCount += 1
+        }, label: {
+            // install sf-symbols first from Apple
+            // then RUN sf-symbols to access database of symbols to choose from
+            Image(systemName:"plus.circle")
+//            VStack {
+//                Text("Add")
+//                Text("Card")
+//            }
+        })
+    }
+    
+    var remove: some View {
+        Button(action: {
+            emojiCount -= 1
+        }, label: {
+            // install sf-symbols first from Apple
+            // then RUN sf-symbols to access database of symbols to choose from
+            Image(systemName:"minus.circle")
+//            VStack {
+//                Text("Remove")
+//                Text("Card")
+//            }
+        })
 
     }
 }
+            
+struct CardView: View {
+    var content: String
+//    var isFaceUp: Bool // if default not provided in declaration, then value must be provided at point of use above
+//    var isFaceUp: Bool { return false } // what is actually happening in line below
+    @State var isFaceUp = false // @State adds a pointer to make this memory indirectly accessed so that it can be updated.
+    
+    var body: some View {
+        ZStack {
+//            var shape: RoundedRectangle = RoundedRectangle(cornerRadius: 20) // can be reduced by using type inference (letting Swift figure out the type)
+//            var shape = RoundedRectangle(cornerRadius: 20) // this should be a "let"
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white) // must fill with a color (in this case "white") otherwise taps can fall through the default "clear color" to the background creating an intermittent experience
+                shape.stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape.fill().foregroundColor(.red)
 
-            
-            
+            }
+
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
             
 
 
@@ -80,5 +101,11 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPod touch (7th generation)")
+        ContentView()
+            .preferredColorScheme(.light)
+            .previewDevice("iPod touch (7th generation)")
     }
 }
+
