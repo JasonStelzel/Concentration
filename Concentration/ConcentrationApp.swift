f54ac3aa97de986b3ef8ca59bbb55bfea17db223
Jason Stelzel <laptop@epigraf.com>
2022-07-26 17:38:28 -0700

    Build basic content view Hello World

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Jason Stelzel on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by Jason Stelzel on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world")
+    }
+}
+
+            
+            
+            
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
