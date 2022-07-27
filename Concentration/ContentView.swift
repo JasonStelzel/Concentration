f5cbbbe9830f9f547670b7e4625688723f9b715e
Jason Stelzel <laptop@epigraf.com>
2022-07-27 13:38:39 -0700

    Build 2-sided flippable CardView array using SwiftUI with filled stroked Rectangle and Text alternative sf-symbols no comments

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -14,11 +14,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-    //            CardView(isFaceUp: false) // Must provide default if value not provided in declaration of isFaceUp in CardView below
                 ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
                     CardView(content: emoji)
                 }
-                
             }
             HStack{
                 remove
@@ -32,19 +30,11 @@ struct ContentView: View {
         .foregroundColor(.red)
     }
     
-    
-//    Button(action: <#T##() -> Void#>, label: <#T##() -> Label#>)
     var add: some View {
             Button(action: {
             emojiCount += 1
         }, label: {
-            // install sf-symbols first from Apple
-            // then RUN sf-symbols to access database of symbols to choose from
             Image(systemName:"plus.circle")
-//            VStack {
-//                Text("Add")
-//                Text("Card")
-//            }
         })
     }
     
@@ -52,13 +42,7 @@ struct ContentView: View {
         Button(action: {
             emojiCount -= 1
         }, label: {
-            // install sf-symbols first from Apple
-            // then RUN sf-symbols to access database of symbols to choose from
             Image(systemName:"minus.circle")
-//            VStack {
-//                Text("Remove")
-//                Text("Card")
-//            }
         })
 
     }
@@ -66,17 +50,12 @@ struct ContentView: View {
             
 struct CardView: View {
     var content: String
-//    var isFaceUp: Bool // if default not provided in declaration, then value must be provided at point of use above
-//    var isFaceUp: Bool { return false } // what is actually happening in line below
-    @State var isFaceUp = false // @State adds a pointer to make this memory indirectly accessed so that it can be updated.
-    
+    @State var isFaceUp = false
     var body: some View {
         ZStack {
-//            var shape: RoundedRectangle = RoundedRectangle(cornerRadius: 20) // can be reduced by using type inference (letting Swift figure out the type)
-//            var shape = RoundedRectangle(cornerRadius: 20) // this should be a "let"
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
-                shape.fill().foregroundColor(.white) // must fill with a color (in this case "white") otherwise taps can fall through the default "clear color" to the background creating an intermittent experience
+                shape.fill().foregroundColor(.white)
                 shape.stroke(lineWidth: 3)
                 Text(content)
                     .font(.largeTitle)
