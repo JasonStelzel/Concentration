2cf909c5705c1ba82e54f3452714c01910671107
Jason Stelzel <laptop@epigraf.com>
2022-07-26 17:41:33 -0700

    5 phases of reducing a Zstack to what is commonly used

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world")
+// closing braces are down below so you can try each Zstack
+// 5th phase (most reduced by removing return and parens)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(lineWidth: 3)
+
+            Text("Hello, Jason!")
+
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+
+// 4th phase (reduce more by using default parameters)
+//        return ZStack() {
+//            RoundedRectangle(cornerRadius: 25)
+//                .stroke(lineWidth: 3)
+//
+//            Text("Hello, Jason!")
+//
+//        }
+//        .padding(.horizontal)
+//        .foregroundColor(.red)
+
+       
+// 3rd phase (reduce with hanging closure)
+//        return ZStack(content: {
+//            RoundedRectangle(cornerRadius: 25)
+//                .stroke(lineWidth: 3)
+//
+//            Text("Hello, Jason!")
+//
+//        })
+//        .padding(.horizontal)
+//        .foregroundColor(.red)
+
+  
+// 2nd phase (add Zstack)
+//        return ZStack(alignment: .center, content: {
+//            RoundedRectangle(cornerRadius: 25)
+//                .stroke(lineWidth: 3)
+//
+//            Text("Hello, Jason!")
+//
+//        })
+//        .padding(.horizontal)
+//        .foregroundColor(.red)
+
+        
+// 1st phase of development
+//        return RoundedRectangle(cornerRadius: 25)
+//            .stroke(lineWidth: 10)
+//            .padding(.horizontal)
+//            .foregroundColor(.orange)
+//
+//        Text("Hello, Jason!").foregroundColor(.orange).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
     }
 }
 
