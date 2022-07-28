f658ae05a336765af4187b753b8decaf3d33a8be
Jason Stelzel <laptop@epigraf.com>
2022-07-28 14:48:35 -0700

    Build game init function with comments

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var emojis = ["🚁", "🚀", "✈️", "⛴", "🛩", "🚗", "🚌", "🚐", "🚛", "🚑", "🚕", "🚚", "🚢", "🚒", "🚓", "🚎", "🚖", "🛵", "🛴", "🛺", "🛻", "🚜"]
-    @State var emojiCount = 12
+    @State var emojiCount = 20
     
     var body: some View {
         VStack {
@@ -21,36 +21,8 @@ struct ContentView: View {
                 }
             }
             .foregroundColor(.red)
-            Spacer()
-            HStack{
-                remove
-                Spacer()
-                add
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
         }
         .padding(.horizontal)
-    }
-    
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-        } label: {
-            Image(systemName:"minus.circle")
-        }
-    }
-
-    var add: some View {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName:"plus.circle")
-        }
     }
 }
             
