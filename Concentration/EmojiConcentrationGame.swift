5c383d695051ca3778869aa032d78a1717bf5c97
Jason Stelzel <laptop@epigraf.com>
2022-07-30 13:49:08 -0700

    Basic skeleton of ConcentrationGame with cards initialized and flipping with card state using Publish, ObservedObject and ObservableObject

--- Concentration/EmojiConcentrationGame.swift
+++ Concentration/EmojiConcentrationGame.swift
@@ -17,13 +17,6 @@ class EmojiConcentrationGame: ObservableObject {
             emojis[pairIndex]
         }
     }
-// this var (var objectWillChange: ObservableObjectPublisher) is generated for free when you declare EmojiConcentrationGame as an ObservableObject
-// You don't need to actually declare it and in fact it will generate an error if you do.
-// You could then fire objectWillChange.send() to announce to the world anytime you change the model (and need something to update on the screen)
-// but it will be automatically fired if you wrap the observed instance of the object with the @Published property wrapper
-// The other parts that go with this are the ObservableObject type (in the ViewModel) and @ObservedObject (in the View) <- ContentView
-// These mirror each other because the @ObservedObject declared in the View (EmojiConcentrationGame) extends ObservableObject in the ViewModel
-// along with this @Published property wrapper (on the model)
        
     @Published private var model: ConcentrationGame<String> = createConcentrationGame()
 
