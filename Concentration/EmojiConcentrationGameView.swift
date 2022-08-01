da9ea2a23c7221edcbdaa688ac5c0d86c81c5796
Jason Stelzel <laptop@epigraf.com>
2022-08-01 00:43:06 -0700

    Alternative game logic using @ViewBuilder to allow conditional constructs inside some view

--- Concentration/EmojiConcentrationGameView.swift
+++ Concentration/EmojiConcentrationGameView.swift
@@ -14,11 +14,15 @@ struct EmojiConcentrationGameView: View {
     
     var body: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3, content: { card in
-            CardView(card: card)
-                .padding(4)
-                .onTapGesture {
-                    game.choose(card)
-                }
+            if card.isMatched && !card.isFaceUp {
+                Rectangle().opacity(0)
+            } else {
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                    }
+            }
         })
         .foregroundColor(.red)
         .padding(.horizontal)
