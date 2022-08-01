e867131e5c06252da2f401af4c3553dfbde79892
Jason Stelzel <laptop@epigraf.com>
2022-08-01 01:40:32 -0700

    Create vector Path shape for card image pie timer function

--- Concentration/EmojiConcentrationGameView.swift
+++ Concentration/EmojiConcentrationGameView.swift
@@ -35,7 +35,7 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth) // stroke INSIDE object vs. half and half with just stroke
-                    Circle()
+                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 115-90)).padding(5).opacity(0.5)
                     Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched {
                     shape.opacity(0)
